20545df34a209629a892484c205990ee025908a8
gabrielqt <gabriel.traiano4@gmail.com>
2026-08-30 15:17:04 -0300

    first commit

---
+++ src/main/java/gabrielqt/pecus/PecusApplication.java
@@ -0,0 +1,13 @@
+package gabrielqt.pecus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabrielqt/pecus/PecusApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrielqt.pecus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
